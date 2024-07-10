9a77c63d706e93d89998efaa34dec9ce274d56bc
pedroaguiarbs <pedroaguiarbs18@gmail.com>
2024-07-10 16:58:51 -0300

    feat: new pages and routes config

---
+++ src/pages/create-trip/steps/invite-guests-step.tsx
@@ -0,0 +1,47 @@
+import { ArrowRight, UserRoundPlus } from "lucide-react";
+
+interface InviteGuestsStepProps {
+  openGuestsModal: () => void;
+  emailsToInvite: string[];
+  openConfirmTripModal: () => void;
+}
+
+export function InviteGuestsStep({
+  openGuestsModal,
+  emailsToInvite,
+  openConfirmTripModal,
+}: InviteGuestsStepProps) {
+  return (
+    <div className="h-16 bg-zinc-900 px-4 rounded-xl flex items-center shadow-shape gap-3">
+      <button
+        type="button"
+        onClick={openGuestsModal}
+        className="flex items-center gap-2 flex-1 text-left"
+      >
+        <UserRoundPlus className="size-5 text-zinc-400" />
+        {emailsToInvite.length > 0 ? (
+          <span>{emailsToInvite.length} pessoa(s) convidada(s)</span>
+        ) : (
+          <span className="text-zinc-400 text-lg flex-1">
+            Quem estará na viagem?
+          </span>
+        )}
+
+        <input
+          type="text"
+          className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+        />
+      </button>
+
+      <div className="w-px h-6 bg-zinc-800" />
+
+      <button
+        onClick={openConfirmTripModal}
+        className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 "
+      >
+        Confirmar viagem
+        <ArrowRight className="size-5" />
+      </button>
+    </div>
+  );
+}
---
+++ src/pages/trip-details/index.tsx
@@ -0,0 +1,70 @@
+import { Calendar, CircleCheck, MapPin, Plus, Settings2 } from "lucide-react";
+
+export function TripDetailsPage() {
+  return (
+    <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
+      <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <MapPin className="size-5 text-zinc-400" />
+          <span className="text-zinc-100">Araguatins, Tocantins</span>
+        </div>
+
+        <div className="flex items-center gap-5">
+          <div className="flex items-center gap-2">
+            <Calendar className="size-5 text-zinc-400" />
+            <span className="text-zinc-100">17 a 23 de Agosto</span>
+          </div>
+
+          <div className="w-px h-6 bg-zinc-800" />
+
+          <button className="bg-zinc-800 text-zinc-200 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-zinc-700 ">
+            Alterar local/data
+            <Settings2 className="size-5" />
+          </button>
+        </div>
+      </div>
+
+      <main className="flex gap-16 px-6">
+        <div className="flex-1 space-y-6">
+          <div className="flex items-center justify-between">
+            <h2 className="text-3xl font-semibold">Atividades</h2>
+            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400 ">
+              <Plus className="size-5" />
+              Cadastrar atividade
+            </button>
+          </div>
+
+          <div className="sapce-y-8">
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 17
+                </span>
+                <span className="text-xs text-zinc-500">Sábado</span>
+              </div>
+              <p>Nenhuma atividade cadastrada nessa data.</p>
+            </div>
+
+            <div className="space-y-2.5">
+              <div className="flex gap-2 items-baseline">
+                <span className="text-xl text-zinc-300 font-semibold">
+                  Dia 18
+                </span>
+                <span className="text-xs text-zinc-500">Domingo</span>
+              </div>
+              <div className="space-y-2.5">
+                <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                  <CircleCheck className="size-5 text-lime-300" />
+                  <span className="text-zinc-100">Academia em grupo</span>
+                  <span className="text-zinc-400 ml-auto">08:00h</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="w-80"></div>
+      </main>
+    </div>
+  );
+}
